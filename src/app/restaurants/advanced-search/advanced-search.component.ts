d7d865dc03d845dec66b26b3114c73f2d091249a
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2021-06-01 22:33:20 +0200

    Add advanced search form validation

--- src/app/restaurants/advanced-search/advanced-search.component.ts
+++ src/app/restaurants/advanced-search/advanced-search.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-advanced-search',
   templateUrl: './advanced-search.component.html',
-  styleUrls: ['./advanced-search.component.scss']
+  styleUrls: ['./advanced-search.component.scss'],
 })
 export class AdvancedSearchComponent implements OnInit {
+  searchForm: any;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.searchForm = new FormGroup({
+      name: new FormControl(null, [Validators.maxLength(100)]),
+      city: new FormControl(null, [Validators.maxLength(35)]),
+      category: new FormControl(null, [Validators.maxLength(50)]),
+      delivery: new FormControl(false),
+    });
   }
 
+  onSearch(): void {
+    console.log(this.searchForm.value);
+  }
 }
