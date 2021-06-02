d240e4e4f9f876f2666aed1438c0461e1610cdbd
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2021-06-02 11:57:29 +0200

    Change type of restaurant id to string

--- src/app/restaurants/restaurants-list/restaurant.model.ts
+++ src/app/restaurants/restaurants-list/restaurant.model.ts
@@ -1,5 +1,5 @@
 import { Address } from "./address";
 
 export class Restaurant{
-    constructor(public id: number, public name: string, public description: string, public category: string, public hasDelivery: boolean, public contactEmail: string, public address: Address){}
+    constructor(public id: string, public name: string, public description: string, public category: string, public hasDelivery: boolean, public contactEmail: string, public address: Address){}
 }
