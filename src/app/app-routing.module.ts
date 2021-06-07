556fc21333d6e2f2e8f9ce6a0ace7bde2c99df55
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2021-06-07 19:32:58 +0200

    Add route for dishes component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DishesComponent } from './dishes/dishes.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/restaurants', pathMatch: 'full' },
   { path: 'restaurants', component: RestaurantsComponent },
+  { path: 'restaurants/:id', component: DishesComponent },
   { path: 'not-found', component: PageNotFoundComponent },
   { path: '**', redirectTo: '/not-found' }
 ];
