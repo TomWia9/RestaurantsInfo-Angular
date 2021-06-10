be8384c8f6d79feb086cb4fe05939d99318f17e6
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2021-06-10 16:24:56 +0200

    Add form template for new restaurant modal

--- src/app/restaurants/new-restaurant-modal/new-restaurant-modal.component.ts
+++ src/app/restaurants/new-restaurant-modal/new-restaurant-modal.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { MdbModalRef } from 'mdb-angular-ui-kit';
 
 @Component({
@@ -6,6 +7,35 @@ import { MdbModalRef } from 'mdb-angular-ui-kit';
   templateUrl: './new-restaurant-modal.component.html',
   styleUrls: ['./new-restaurant-modal.component.scss']
 })
-export class NewRestaurantModalComponent {
+export class NewRestaurantModalComponent implements OnInit {
+  form: FormGroup;
+  success = false;
+  error = '';
+
   constructor(public modalRef: MdbModalRef<NewRestaurantModalComponent>) {}
+
+  ngOnInit(): void {
+    this.createForm();
+  }
+
+  onSubmit(): void {
+    console.log(this.form.value);
+  }
+
+  private createForm(): void {
+    this.form = new FormGroup({
+      name: new FormControl(''),
+      description: new FormControl(''),
+      category: new FormControl(''),
+      hasDelivery: new FormControl(false),
+      contactNumber: new FormControl(''),
+      contactEmail: new FormControl(''),
+      address: new FormGroup({
+        city: new FormControl(''),
+        street: new FormControl(''),
+        houseNumber: new FormControl(''),
+        postalCode: new FormControl('')
+      })
+    });
+  }
 }
