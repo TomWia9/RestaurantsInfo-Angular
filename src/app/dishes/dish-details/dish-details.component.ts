ef4e78f0d94c8e6c3d345bb83e383c2249d93ef7
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2021-06-15 13:01:19 +0200

    Add edit dish possibility

--- src/app/dishes/dish-details/dish-details.component.ts
+++ src/app/dishes/dish-details/dish-details.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { MdbModalRef, MdbModalService } from 'mdb-angular-ui-kit';
 import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/auth/auth.service';
+import { User } from 'src/app/auth/user.model';
 import { Dish } from '../dish.model';
 import { DishesService } from '../dishes.service';
+import { EditDishModalComponent } from '../edit-dish-modal/edit-dish-modal.component';
 
 @Component({
   selector: 'app-dish-details',
@@ -13,9 +17,15 @@ export class DishDetailsComponent implements OnInit, OnDestroy {
   restaurantId = '';
   dish: Dish;
   dishSubscription: Subscription;
+  userSubscription: Subscription;
+  modalRef: MdbModalRef<EditDishModalComponent>;
+  admin = false;
+
   constructor(
     private dishesService: DishesService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private modalService: MdbModalService,
+    private authService: AuthService
   ) {}
 
   ngOnInit(): void {
@@ -27,6 +37,22 @@ export class DishDetailsComponent implements OnInit, OnDestroy {
         .subscribe((dish) => {
           this.dish = dish;
         });
+    });
+
+    this.userSubscription = this.authService.user.subscribe((user: User) => {
+      if (user) {
+        this.admin = user.role === 'Administrator';
+      }
+    });
+  }
+
+  onUpdateDish(): void {
+    this.modalRef = this.modalService.open(EditDishModalComponent, {
+      data: {
+        updateMode: true,
+        restaurantId: this.restaurantId,
+        dishId: this.dish.id
+      }
     });
   }
 
